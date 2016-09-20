17a7d1c32c44af3609b08ed31c4527bfcaa1ffef
Jakub Rak <theaeroson@gmail.com>
2016-09-20 20:32:11 +0200

    no message

--- TawGatherMembersInfo/Models/AttendanceReport.sql
+++ TawGatherMembersInfo/Models/AttendanceReport.sql
@@ -26,8 +26,23 @@ begin
 		)
 		group by p.PersonId
 		order by name;
-
+        
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET cursor_end = 1;
+    
+	CREATE TEMPORARY TABLE IF NOT EXISTS attendanceReportResult (
+		UnitName varchar(100),
+        UserName varchar(500),
+        RankNameShort varchar(10),
+        Trainings bigint(20),
+        Attended bigint(20),
+        Excused bigint(20),
+        AWOL bigint(20),
+        MandatoryAVG float,
+		TotalAVG float,
+        DaysInRank bigint(20)
+	);
+    
+    TRUNCATE TABLE attendanceReportResult;
     
 	open selected_people;
 	REPEAT
@@ -36,35 +51,34 @@ begin
         		
         -- WHERE exec_datetime BETWEEN DATE_SUB(NOW(), INTERVAL daysBack DAY) AND NOW();        
 		
-        select 
+        insert into attendanceReportResult values (
         
-			(select u.Name from Units u where u.TawId = rootUnitId) as UnitName,
-            (select p.Name from People p where p.PersonId = selected_PersonId) as UserName,
-            (select p.RankNameShort from People p where p.PersonId = selected_PersonId) as RankNameShort,                      
+			(select u.Name from Units u where u.TawId = rootUnitId),
+            (select p.Name from People p where p.PersonId = selected_PersonId),
+            (select p.RankNameShort from People p where p.PersonId = selected_PersonId),                      
             
-            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId) as Trainings,
+            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId),
             
-            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 1) as Attended,
+            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 1),
             
-            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 2) as Excused,   
+            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 2),
             
-            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 3) as AWOL,
+            (select count(*) from People p join PeopleToEvents pe on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 3),
 		
 			(select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 1 join Events e on e.Mandatory and e.EventId = pe.EventId) -- total mandatories attended
-			/ (select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId join Events e on e.Mandatory and e.EventId = pe.EventId) -- total mandatories
-			as MandatoryAVG,
+			/ GREATEST(1, (select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId join Events e on e.Mandatory and e.EventId = pe.EventId) ), -- total mandatories			
 			
 			(select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId and pe.AttendanceType = 1) -- total any events attended
-			/ (select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId) -- total any events
-			as TotalAVG,
+			/ GREATEST(1, (select count(*) from PeopleToEvents pe join People p on p.PersonId = selected_PersonId and p.PersonId = pe.PersonId) ), -- total any events
 			
-            0 as DaysInRank
-		;
+            0
+		);
         
 		UNTIL cursor_end = 1
 	END REPEAT;
     close selected_people;
     
+    select * from attendanceReportResult;
 
 
 end//
